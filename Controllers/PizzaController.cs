5b41f21de4d8641fcbe558178024a068d8082fa2
DNParadox <m96fr@DESKTOP-NBI01C4>
2022-11-22 15:58:50 +0100

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,131 @@
+﻿using la_mia_pizzeria_static.Models;
+using la_mia_pizzeria_static.data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.Extensions.Hosting;
+
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        PizzeriaDbContext db;
+
+        public PizzaController() : base()
+        {
+            // Usiamo Data quindi DB
+            // Metodo mostrato da Paolo per dichiarare soltanto una volta il nostro DB anziché di volta in volta 
+            db = new PizzeriaDbContext();
+        }
+        //Read 
+        public IActionResult Index()
+        {
+            // Usiamo Data quindi DB . Metodo Vecchio dove bisognava dichiarare di volta in volta il db in uso
+            //PizzeriaDbContext db = new PizzeriaDbContext();
+
+
+            // Usiamo Model
+            List<Pizza> Pizzas = db.Pizzas.ToList();
+
+
+            // In return possiamo passare un singolo argomento
+            return View(Pizzas);
+        }
+
+        public IActionResult Detail(int id)
+        {
+
+
+            Pizza Pizzas = db.Pizzas.Where(p => p.Id == id).FirstOrDefault();
+
+            return View(Pizzas);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]  // Equivalente di CSRF di Laravel
+        public IActionResult Create(Pizza Pizzas)
+        {
+            if (!ModelState.IsValid)
+            {
+                //return View(post);
+                return View();
+            }
+
+            // Pusha nel db con .add
+            db.Pizzas.Add(Pizzas);
+            //Salva i cambiamenti effettuati
+            db.SaveChanges();
+
+
+            //Redirect alla Index quindi alla lista di pizze creata
+            return RedirectToAction("Index");
+        }
+
+
+
+
+        public IActionResult Update(int id)
+        {
+            Pizza Pizzas = db.Pizzas.Where(p => p.Id == id).FirstOrDefault();
+
+            if (Pizzas == null)
+                return NotFound();
+
+            //return View() --> non funziona perchè non ha la memoria della Pizzas
+            return View(Pizzas);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(int id, Pizza formData)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                //return View(post);
+                return View();
+            }
+
+            Pizza Pizzas = db.Pizzas.Where(p => p.Id == id).FirstOrDefault();
+
+            if (Pizzas == null)
+            {
+                return NotFound();
+            }
+
+            Pizzas.Title = formData.Title;
+            Pizzas.Description = formData.Description;
+            Pizzas.Image = formData.Image;
+            Pizzas.Price = formData.Price;
+
+            //db.Posts.Update(post);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            Pizza Pizzas = db.Pizzas.Where(p => p.Id == id).FirstOrDefault();
+
+            if (Pizzas == null)
+            {
+                return NotFound();
+            }
+
+            db.Pizzas.Remove(Pizzas);
+            db.SaveChanges();
+
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,45 @@
+﻿
+
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        public string Title { get; set; }
+
+
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [Column(TypeName = "text")]
+        public string Description { get; set; }
+
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        public string Image { get; set; }
+
+
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [Range(6, 30, ErrorMessage = "Il prezzo non deve essere inferiore a 6 euro")]
+        public float Price { get; set; }
+
+
+        public Pizza ()
+        {
+
+        }
+        public Pizza(string title, string description, string image, float price)
+        {
+            this.Title = title;
+            this.Description = description;
+            this.Image = image;
+            this.Price = price;
+        }
+    }
+}
